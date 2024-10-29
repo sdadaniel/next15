d8643d4d279da09b8e04f4cec315f7762b97917c
sdadaniel <sdadaniel@hanmail.net>
2024-10-29 09:09:54 +0900

    feat: 기본 테스트 코드 작성

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,14 +1,24 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { describe } from "node:test";
-import Page from "../app/page";
+import Page from "./page";
 
 describe("Page", () => {
+  let unmount: ReturnType<typeof render>["unmount"];
+
+  beforeEach(() => {
+    const renderResult = render(<Page />);
+    unmount = renderResult.unmount;
+  });
+
+  afterEach(() => {
+    unmount();
+  });
+
   it("renders a heading", () => {
-    render(<Page />);
-
-    const heading = screen.getByRole("heading", { level: 1 });
-
-    expect(heading).toBeInTheDocument();
+    const Home1 = screen.getByRole("heading", { name: "Home1", level: 1 });
+    const Home2 = screen.getByRole("heading", { name: "Home2", level: 3 });
+    expect(Home2).toBeInTheDocument();
+    expect(Home1).not.toHaveStyle({ color: "blue" });
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Page() {
+  const [disabled, setDisabled] = useState<boolean>(false);
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home1</h1>
+      <h3 style={{ color: "blue" }}>Home2</h3>
       <Link href="/about">About</Link>
+
+      <button
+        disabled={disabled}
+        onClick={() => {
+          setDisabled((p) => !p);
+        }}
+      >
+        Click
+      </button>
+
+      <button
+        disabled={disabled}
+        onClick={() => {
+          setDisabled((p) => !p);
+        }}
+      >
+        두번째
+      </button>
     </div>
   );
 }
